7111d6092fc9180ad35e279307ba86193f4c2f16
Jatin Gidwani <jatin11gidwani@gmail.com>
2018-04-24 00:23:07 +0530

    Create new starter files for different React components
    Created new starter files for different react components including FAQs,
    GameDisplay, HowToPlay, NavigationMenu, QuoteAndOptions and Score.

---
+++ client/src/components/FAQs/FAQs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './FAQs.css'
+
+class FAQs extends Component {
+ 
+  render () {
+    return (
+      <div> FAQs </div>    
+    );
+  };
+};
+
+export default FAQs;
---
+++ client/src/components/GameDisplay/GameDisplay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './GameDisplay.css'
+
+class GameDisplay extends Component {
+ 
+  render () {
+    return (
+      <div> GameDisplay </div>    
+    );
+  };
+};
+
+export default GameDisplay;
